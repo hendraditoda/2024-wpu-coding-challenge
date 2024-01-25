f4c193a4f455d19a1aa1d7a08c7370ce5cb8ab13
Hendradito Dwi A <hendradito21@gmail.com>
2024-01-25 10:47:43 +0700

    day #3

---
+++ 3-hero.js
@@ -0,0 +1,20 @@
+// WPU Coding Challenge 2024
+// 3/366
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+// function hero(bullets, dragons) {
+//   //Get Coding!
+//   if (bullets / 2 >= dragons) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+function hero(bullets, dragons) {
+  return bullets / 2 >= dragons ? true : false;
+}
+
+const hero = (bullets, dragons) => bullets / 2 >= dragons;
+
+console.log(hero(7, 5));
