fa08db2d5bed8f1de43de7f573db304b280d533d
Hendradito Dwi A <hendradito21@gmail.com>
2024-01-25 10:08:24 +0700

    challenge 2/366

---
+++ 2-reverseSeq.js
@@ -0,0 +1,16 @@
+// WPU Coding Challenge 2024
+// 2/366
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d/javascript
+
+// const reverseSeq = (n) => {
+//   const result = [];
+//   for (let i = n; i >= 1; i--) {
+//     result.push(i);
+//   }
+//   return result;
+// };
+// const reverseSeq = (n) => [...Array(n)].map((el, i) => i + 1).reverse();
+
+const reverseSeq = (n) => [...Array(n)].map((el, i) => n - i);
+
+console.log(reverseSeq(5));
