b7950074c38fb45f20b2d2e9b5d14772482a2033
Hendradito Dwi A <hendradito21@gmail.com>
2024-02-05 08:46:21 +0700

    day 10/366

---
+++ 10-convertString.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024
+// 9/366
+// https://www.codewars.com/kata/577a98a6ae28071780000989
+
+function stringToArray(string) {
+  return string.split(' ');
+}
+
+const stringToArray = (string) => string.split(' ');
+
+console.log(stringToArray('Robin Singh'), ['Robin', 'Singh']);
