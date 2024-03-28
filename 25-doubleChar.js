1364db41bbdc1ffa37add23edcf2cdcc35660af7
Hendradito Dwi A <hendradito21@gmail.com>
2024-03-28 12:10:01 +0700

    Day 25/366

---
+++ 25-doubleChar.js
@@ -0,0 +1,23 @@
+// WPU Coding Challenge 2024
+// 25/366
+// https://www.codewars.com/kata/56b1f01c247c01db92000076
+
+// function doubleChar(str) {
+//   let result = '';
+//   for (let i = 0; i < str.length; i++) {
+//     result += str[i];
+//     result += str[i];
+//   }
+// }
+
+// function doubleChar(str) {
+//   return [...str].map((el) => el + el).join('');
+// }
+
+const doubleChar = (str) =>
+  str
+    .split('')
+    .map((el) => el + el)
+    .join('');
+
+console.log(doubleChar('Adidas'));
