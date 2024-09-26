fe605e193b9b856e5ee80c2192d9226dd7e218c6
Hendradito Dwi A <hendradito21@gmail.com>
2024-09-26 11:19:32 +0700

    Day 30/366

---
+++ 30-enough.js
@@ -0,0 +1,24 @@
+// WPU Coding Challenge 2024
+// 30/366
+// https://www.codewars.com/kata/5875b200d520904a04000003
+
+// function enough(cap, on, wait) {
+//   if (cap >= on + wait) {
+//     return 0;
+//   } else {
+//     return wait - (cap - on);
+//   }
+// }
+
+// function enough(cap, on, wait) {
+//   return cap >= on + wait ? 0 : wait - (cap - on);
+// }
+
+// function enough(cap, on, wait) {
+//   return cap >= on + wait ? 0 : wait - (cap - on);
+// }
+
+const enough = (cap, on, wait) => (cap >= on + wait ? 0 : wait - (cap - on));
+
+console.log(enough(10, 5, 5));
+console.log(enough(75, 70, 46));
