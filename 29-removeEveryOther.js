e410c0e272111f58f27310cb4f50d9dd55f02a63
Hendradito Dwi A <hendradito21@gmail.com>
2024-09-26 10:48:35 +0700

    Day 29/366

---
+++ 29-removeEveryOther.js
@@ -0,0 +1,23 @@
+// WPU Coding Challenge 2024
+// 29/366
+// https://www.codewars.com/kata/5769b3802ae6f8e4890009d2
+
+// function removeEveryOther(arr) {
+//   const newArr = [];
+//   for (let i = 0; i < arr.length; i += 2) {
+//     newArr.push(arr[i]);
+//   }
+//   return newArr;
+// }
+
+// function removeEveryOther(arr) {
+//   return arr.filter((el, i) => i % 2 == 0);
+// }
+
+// function removeEveryOther(arr) {
+//   return arr.filter((el, i) => i % 2 == 0);
+// }
+
+const removeEveryOther = (arr) => arr.filter((el, i) => i % 2 == 0);
+
+console.log(removeEveryOther(["Hello", "Goodbye", "Hello Again"]));
